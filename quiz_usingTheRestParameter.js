5bd1a9d2ada1b4469c8e7597bbd1e261defe102e
hanapotski <hannahjoy.zulueta@gmail.com>
2017-10-14 22:40:41 -0700

    Quiz 1-5 - Using the Rest Parameter

---
+++ quiz_usingTheRestParameter.js
@@ -0,0 +1,22 @@
+/*
+ * Programming Quiz: Using the Rest Parameter (1-5)
+ */
+
+// your code goes here
+
+function average(...nums) {
+    let sum = 0, ave = 0, length = nums.length;
+    if (length === 0){
+        return 0;
+    } else {
+        for (const num of nums){
+            sum += num;
+        }
+        return sum/length
+    }
+}
+
+console.log(average(2, 6));
+console.log(average(2, 3, 3, 5, 7, 10));
+console.log(average(7, 1432, 12, 13, 100));
+console.log(average());
