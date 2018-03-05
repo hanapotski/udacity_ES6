16adafd984adae5d939a7467b37483b4f05716d0
hanapotski <hannahjoy.zulueta@gmail.com>
2018-03-05 11:50:06 -0800

    Quiz - Using Sets

---
+++ Lesson3/quiz_usingSets.js
@@ -0,0 +1,29 @@
+Directions:
+Create a variable with the name myFavoriteFlavors and give it the value of an empty Set object. Then use the .add() method to add the following strings to it:
+
+"chocolate chip"
+"cookies and cream"
+"strawberry"
+"vanilla"
+Then use the .delete() method to remove "strawberry" from the set.
+
+/*
+ * Programming Quiz: Using Sets (3-1)
+ *
+ * Create a Set object and store it in a variable named `myFavoriteFlavors`. Add the following strings to the set:
+ *     - chocolate chip
+ *     - cookies and cream
+ *     - strawberry
+ *     - vanilla
+ *
+ * Then use the `.delete()` method to remove "strawberry" from the set.
+ */
+
+let myFavoriteFlavors = new Set();
+
+myFavoriteFlavors.add('chocolate chip');
+myFavoriteFlavors.add('cookies and cream');
+myFavoriteFlavors.add('strawberry');
+myFavoriteFlavors.add('vanilla');
+
+myFavoriteFlavors.delete('strawberry');
